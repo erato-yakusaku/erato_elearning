145dc51ce6636ffb22dc2ed99914fd4d31fa8d18
Tome Hirata <humanland106@gmail.com>
2019-11-02 20:50:17 +0900

    homepageからblogindexの一覧が見れるように

--- home/models.py
+++ home/models.py
@@ -3,11 +3,20 @@ from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
+from blog.models import BlogIndexPage
 
 
 
 class HomePage(Page):
     body = RichTextField(blank=True)
+    max_count = 1
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        blogindexpages = BlogIndexPage.objects.live()
+        context['blogindexpages'] = blogindexpages
+        print(blogindexpages)
+        return context
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
